9f96638f3d03dba979aa67c4a0f6c55a82bb88d9
Vincent Archambault-Bouffard <vincentarchambault@icloud.com>
2022-09-19 16:08:03 -0400

    Add moves. Refined definition of RubikCube

--- src/Rubik.hs
+++ src/Rubik.hs
@@ -1,10 +1,14 @@
 module Rubik
     ( 
         Color(..),
-        RubikCube(..),
         RubikFace,
-        getFaces,
-        solvedCube
+        faceToList,
+        RubikCube(..),
+        solvedCube,
+        Rotation(..),
+        Move(..),
+        move,
+        moves
     ) where
 
 import Data.List (intercalate)
@@ -12,7 +16,7 @@ import Data.List (intercalate)
 -- A rubik cube is made of 6 faces, each one has 9 tiles. Each tile can have one
 -- of the six possible colors.
 data Color = White | Red | Blue | Yellow | Orange | Green
-    deriving (Eq, Ord)
+    deriving (Eq)
 
 instance Show Color where
     show White = "W"
@@ -28,20 +32,36 @@ instance Show Color where
 -- advantage of that to define a coordinate system that is independant of how
 -- someone holds the rubik cude.
 
--- We simple define a rubik cube has the list of all 54 faces. The first 9 tiles
+-- We simple define a rubik cube has a list of all 54 faces. The first 9 tiles
 -- are is the white face, the second the red face, third blue face and so on.
-data RubikCube = RubikCube [Color] -- A list of length 54
-                deriving (Eq)
+-- We use tuples 
 
-type RubikFace = [Color] -- A list of length 9
+-- A face has 9 tiles
+type RubikFace = (Color, Color, Color,
+                  Color, Color, Color,
+                  Color, Color, Color)
 
-getFaces :: RubikCube -> (RubikFace, RubikFace, RubikFace, RubikFace, RubikFace, RubikFace)
-getFaces (RubikCube xs) = toTuple $ go xs
-    where go [] = []
-          go ys = take 9 ys : go (drop 9 ys)
+faceToList :: RubikFace -> [Color]
+faceToList (a,b,c,d,e,f,g,h,i) = [a,b,c,d,e,f,g,h,i]
 
-          toTuple [a,b,c,d,e,f] = (a,b,c,d,e,f)
-          toTuple _ = error "RubikCube wrong list size"
+data RubikCube = RubikCube {
+    whiteFace :: RubikFace,
+    redFace :: RubikFace,
+    blueFace :: RubikFace,
+    yellowFace :: RubikFace,
+    orangeFace :: RubikFace,
+    greenFace :: RubikFace
+    }
+    deriving (Eq)
+
+
+-- getFaces :: RubikCube -> (RubikFace, RubikFace, RubikFace, RubikFace, RubikFace, RubikFace)
+-- getFaces (RubikCube xs) = toTuple $ go xs
+--     where go [] = []
+--           go ys = take 9 ys : go (drop 9 ys)
+
+--           toTuple [a,b,c,d,e,f] = (a,b,c,d,e,f)
+--           toTuple _ = error "RubikCube wrong list size"
 
 -- Prints a rubik cube as a 2D projection
 -- as if white is the front face
@@ -62,9 +82,8 @@ getFaces (RubikCube xs) = toTuple $ go xs
 --         6 7 8
 
 printRubikCube :: RubikCube -> String
-printRubikCube xs =
-    let (w,r,b,y,o,g) = getFaces xs
-        top :: [String]
+printRubikCube (RubikCube w r b y o g) =
+    let top :: [String]
         top =  rubikFaceMatrix r
         center :: [String]
         center = concatMatrix 
@@ -80,7 +99,7 @@ printRubikCube xs =
     where
         rubikFaceMatrix :: RubikFace -> [String]
         rubikFaceMatrix u =
-            let colors = map show u
+            let colors = map show $ faceToList u
                 firstRow = take 3 colors
                 secondRow = take 3 $ drop 3 colors
                 thirdRow = drop 6 colors
@@ -100,32 +119,128 @@ instance Show RubikCube where
 -- The initial cube is well sorted
 solvedCube :: RubikCube
 solvedCube = RubikCube 
-            $ concatMap (\c -> replicate 9 c) [White, Red, Blue, Yellow, Orange, Green]
+             (mkFace White) 
+             (mkFace Red)
+             (mkFace Blue) 
+             (mkFace Yellow) 
+             (mkFace Orange) 
+             (mkFace Green)
+
+    where mkFace :: Color -> RubikFace
+          mkFace c = (c,c,c,c,c,c,c,c,c)
+
+-- Moves. There is 3 axis and 3 columns for each axis to move. Each column can
+-- move clockwise or counterclockwise in relation to the axis. So 18 moves in
+-- total. But there is only 6 basics moves, the 12 others are a combination of
+-- those 6. So we first define the 6 basics moves and then from them other 12.
+-- Each move is equivalent to rotating the appropriate face (bases on the color)
+-- clockwise when view the face up front
 
 
--- Swap two element of the list, the list must be long enough
-swap :: Int -> Int -> [a] -> [a]
-swap x y ls | x > y = swap y x ls
-swap x y ls | x == y = ls
-swap x y ls =
-    let (start, ls1) = splitAt x ls 
-        x' = head ls1
-        (middle, ls2) = splitAt (y - x - 1) (tail ls1)
-        y' = head ls2
-    in start ++ [y'] ++ middle ++ [x'] ++ tail ls2 
+-- Rotate clockwise a RubikFace
+rotateFaceCWise :: RubikFace -> RubikFace
+rotateFaceCWise (f1,f2,f3,f4,f5,f6,f7,f8,f9) = 
+            --    ^  ^  ^  ^  ^  ^  ^  ^  ^
+                (f7,f4,f1,f8,f5,f2,f9,f6,f3)
 
--- Permute many elements, the first taking the place of the second and so forth
--- until the last takes the place of the first
-permute :: [Int] -> [a] -> [a]
-permute [] ls = ls
-permute (x : xs) ls = foldl swap' ls xs
-    where
-    swap' :: [a] -> Int -> [a]
-    swap' ls b = swap x b ls
+-- Rotate anti clockwise a RubikFace
+rotateFaceAntiCWise :: RubikFace -> RubikFace
+rotateFaceAntiCWise = rotateFaceCWise . rotateFaceCWise . rotateFaceCWise
 
--- move :: Move -> RubikCube -> RubikCube
--- move FCw (RubikCube xs) = RubikCube $
---      foldl permute' xs [[1,3,9,7],[2,6,8,4],[]]
---     where
---     permute' :: [Color] -> [Int] -> [Color]
---     permute' ls xs = permute xs ls
+-- Rotate a RubikFace half a turn
+rotateHalfTurn :: RubikFace -> RubikFace
+rotateHalfTurn = rotateFaceCWise . rotateFaceCWise
+
+-- Rotates the four neighbours of the white face clockwise
+rotateWhiteSides :: (RubikFace, RubikFace, RubikFace, RubikFace)
+                 -> (RubikFace, RubikFace, RubikFace, RubikFace)
+rotateWhiteSides ((r0,r1,r2,r3,r4,r5,r6,r7,r8),
+                  (g0,g1,g2,g3,g4,g5,g6,g7,g8),
+                  (o0,o1,o2,o3,o4,o5,o6,o7,o8),
+                  (b0,b1,b2,b3,b4,b5,b6,b7,b8)) =
+                 
+                 ((r0,r1,r2,r3,r4,r5,b8,b5,b2),
+                  (r6,g1,g2,r7,g4,g5,r8,g7,g8),
+                  (g6,g3,g0,o3,o4,o5,o6,o7,o8),
+                  (b0,b1,o0,b3,b4,o1,b6,b7,o2))
+
+-- Apply a basic move to a rubik cube. Rotating clockwise the face with same
+-- color as the first parameter
+basicMove :: Color -> RubikCube -> RubikCube
+basicMove White (RubikCube w r b y o g) =
+    let w' = rotateFaceCWise w
+        (r',g',o',b') = rotateWhiteSides (r,g,o,b)
+    in RubikCube w' r' b' y o' g'
+basicMove Red (RubikCube w r b y o g) =
+    let r' = rotateFaceCWise r
+        -- When computing the neighbours, we can pretend that red is the front
+        -- face (at the place of white) and reuse rotateWhiteSides. But blue and
+        -- green are rotated to take into account that the cube rotated
+        (y',g',w',b') = rotateWhiteSides (y,rotateFaceAntiCWise g,w,rotateFaceCWise b)
+    in RubikCube w' r' (rotateFaceAntiCWise b') y' o (rotateFaceCWise g')
+basicMove Blue (RubikCube w r b y o g) =
+    let b' = rotateFaceCWise b
+        -- We use the same trick as for MRed
+        (r',w',o',y') = rotateWhiteSides (rotateFaceAntiCWise r,w,rotateFaceCWise o, rotateHalfTurn y)
+    in RubikCube w' (rotateFaceCWise r') b' (rotateHalfTurn y') (rotateFaceAntiCWise o') g
+basicMove Yellow (RubikCube w r b y o g) = 
+    let y' = rotateFaceCWise y
+        -- We use the same trick as for MRed
+        (r',b',o',g') = rotateWhiteSides (rotateHalfTurn r, b,rotateHalfTurn o, g)
+    in RubikCube w (rotateHalfTurn r')  b' y' (rotateHalfTurn o') g'
+basicMove Orange (RubikCube w r b y o g) = 
+    let o' = rotateFaceCWise o
+        -- We use the same trick as for MRed
+        (w',g',y',b') = rotateWhiteSides (w,rotateFaceCWise g,y, rotateFaceAntiCWise b)
+    in RubikCube w' r (rotateFaceCWise b') y' o' (rotateFaceAntiCWise g')
+basicMove Green (RubikCube w r b y o g) = 
+    let g' = rotateFaceCWise g
+        -- We use the same trick as for MRed
+        (r',y',o',w') = rotateWhiteSides (rotateFaceCWise r,rotateHalfTurn y,rotateFaceAntiCWise o,w)
+    in RubikCube w' (rotateFaceAntiCWise r') b (rotateHalfTurn y') (rotateFaceCWise o') g'
+
+-- A rotation can be clockwise or anticlockwise
+data Rotation = CWise | AntiCWise
+    deriving (Eq)
+
+instance Show Rotation where
+    show CWise = "CW"
+    show AntiCWise = "ACW"
+
+instance Read Rotation where
+    readsPrec _ ('C' :'W' : xs)  = [(CWise, xs)]
+    readsPrec _ ('A' : 'C' :'W' : xs) = [(AntiCWise, xs)]
+    readsPrec _ _ = []
+
+-- We can move each face clock wise or anti clock wise
+data Move = Move Color Rotation
+    deriving (Eq)
+
+instance Show Move where
+    show (Move c CWise) = show c
+    show (Move c AntiCWise) = show c ++ "'"
+
+instance Read Move where
+    readsPrec _ ('W' : '\'' : xs)  = [(Move White AntiCWise, xs)]
+    readsPrec _ ('W' : xs)  = [(Move White CWise, xs)]
+    readsPrec _ ('R' : '\'' : xs)  = [(Move Red AntiCWise, xs)]
+    readsPrec _ ('R' : xs)  = [(Move Red CWise, xs)]
+    readsPrec _ ('B' : '\'' : xs)  = [(Move Blue AntiCWise, xs)]
+    readsPrec _ ('B' : xs)  = [(Move Blue CWise, xs)]
+    readsPrec _ ('Y' : '\'' : xs)  = [(Move Yellow AntiCWise, xs)]
+    readsPrec _ ('Y' : xs)  = [(Move Yellow CWise, xs)]
+    readsPrec _ ('O' : '\'' : xs)  = [(Move Orange AntiCWise, xs)]
+    readsPrec _ ('O' : xs)  = [(Move Orange CWise, xs)]
+    readsPrec _ ('G' : '\'' : xs)  = [(Move Green AntiCWise, xs)]
+    readsPrec _ ('G' : xs)  = [(Move Green CWise, xs)]
+    readsPrec _ _ = []
+
+-- Similar to a basic move, but can rotate anti clockwise
+move :: Move -> RubikCube -> RubikCube
+move (Move c CWise) rc = basicMove c rc
+move (Move c AntiCWise) rc = m $ m $ m rc
+    where m = basicMove c
+
+-- Apply a series of move together
+moves :: [Move] -> RubikCube -> RubikCube
+moves ms rc = foldl (flip move) rc ms
